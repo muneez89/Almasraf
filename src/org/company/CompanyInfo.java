67cd1e3aee2486e9a47592aed0b6aae281abbb44
muneez89 <muneez89@gmail.com>
2021-12-06 15:32:29 +0530

    Folder

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,33 @@
+package org.company;
+
+public class CompanyInfo {
+
+	
+	private void companyName(int id) {
+	System.out.println("Company Id:" +id);
+
+	}
+	
+	private void companyName(String name,String location,int strength) {
+		
+		System.out.println("Company Name" +name);
+		System.out.println("Company location" +location);
+		System.out.println("Company Strength" +strength);
+
+	}
+	
+	private void companyName(long comCode,int branches) {
+		System.out.println("Company code" +comCode);
+		System.out.println("Company code" +branches);
+	
+
+	}
+	
+	public static void main(String[] args) {
+		
+		CompanyInfo c = new CompanyInfo();
+		c.companyName(45685);
+		c.companyName("HCL","Chennai",1500);
+		c.companyName(982411111412252l,12);
+	}
+}
